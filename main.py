fd2a4aa556477112c2fd31a30a919232171c58cb
Eduardo Magalhães <110843321+EduardoMagalhaes10@users.noreply.github.com>
2025-12-11 00:18:43 +0000

    Classe produto criada com sucesso!

---
+++ main.py
@@ -0,0 +1,10 @@
+class Produto:
+    def __init__(self, nome, preco, tipo_venda):
+        self.nome = nome
+        self.preco = float(preco)
+        self.tipo_venda = tipo_venda
+        self.agricultor = None
+
+    def __str__(self):
+        return (f"Produto: {self.nome} Preço: R${self.preco:.2f}"
+                f"Tipo de Venda: {self.tipo_venda}")
