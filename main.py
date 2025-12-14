6cecb4eafc0167d7f836e4f15a55b9644f53fd55
DANILO NOVAES <danilo.barrosnovaes07@gmail.com>
2025-12-14 14:51:02 +0000

    Criação Classse Adm e algumas definições

--- main.py
+++ main.py
@@ -22,4 +22,21 @@ class Agricultor:
     def __str__(self):
         return f"Agricultor: {self.nome} Endereço: {self.endereco}"
 
+class Adm:
+    def __init__(self):
+        self.agricultores = {}
+        self.produtos = {}
 
+    def cadastrar_agricultor(self):
+        print("--- Cadastro de Agricultor ---")
+        nome = input("Digite o nome do Agricultor: ")
+        endereco = input("Digite o endereço do Agricultor: ")
+
+        if nome in self.agricultores:
+            print(f"O agricultor {nome} já está cadastrado.")
+            return
+
+        novo_agricultor = Agricultor(nome, endereco)
+        self.agricultores[nome] = novo_agricultor
+        print(f"Agricultor {nome} cadastrado com sucesso!")
+        return novo_agricultor
